216b043f562a512002b151717bd242e406f3eca3
Eric Ho <endonoho@gmail.com>
2020-09-29 14:57:51 -0700

    Practise Hashes and Caching

--- redis-app/routes/web.php
+++ redis-app/routes/web.php
@@ -11,38 +11,37 @@
 |
 */
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Route;
 
-Route::get('articles/trending', function () {
-    // Get the top 3 trending articles
-    $trending = Redis::zrevrange('trending_articles', 0, 2);
+Route::get('/', function () {
+    $user3Stats = [
+        'favorites' => 10,
+        'watchLaters' => 20,
+        'completions' => 35
+    ];
+    // Add hash to database
+    Redis::hmset('user.3.stats', $user3Stats);
+    // Display to screen
+    return Redis::hgetall('user.3.stats');
 
-    // Fetch all articles by id
-    App\Article::whereIn('id', $trending);
+    // Cache
+    Cache::put('foo', 'bar', 10);
+    return Cache::get('foo');
 
-    // Decode json and rehyrate
-    // hyrdate accepts arr of records, filters each one, creates new instance while passing through the attributes
-    App\Article::hydrate(
-        array_map('json_decode', $trending)
-    );
-
-    // You may also create a global query scope
-    App\Article::hydrateFromJsonString($trending);
-
-    return $trending;
+    // Return favorites count for this user
+    return Redis::hgetall('user.1.stats')['favorites'];
 });
 
-Route::get('articles/{article}', function (App\Article $article) {
-    // increment trending_articles score by 1, set member to the article id
-    Redis::zincrby('trending_articles', 1, $article->id);
+Route::get('users/{id}/stats', function ($id) {
+    // Fetch stats from the user with the given id
+    return Redis::hgetall("user.${id}.stats");
+});
 
-    // If you don't want to store the id, you can store the article which will be cast to json in redis-cli
-    Redis::zincrby('trending_articles', 1, $article);
+Route::get('favorite-video', function () {
+    // Increment counter
+    Redis::hincrby('user.1.stats', 'favorites', 1);
 
-    // Remove items from trending_articles sorted set and retain the top 3 items
-    // You can go to kernel.php to set up a schedule redis command that runs once a week/month
-    Redis::zremrangebyrank('trending_articles', 0, -4);
-
-    return $article;
+    return redirect('/');
 });
