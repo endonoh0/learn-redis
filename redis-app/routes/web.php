a4ef94edd6b8311fdce4c44f7c3580429c8957a5
Eric Ho <endonoho@gmail.com>
2020-09-29 15:36:03 -0700

    Refactor caching logic to Articles class

--- redis-app/routes/web.php
+++ redis-app/routes/web.php
@@ -1,21 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|----------------- ---------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return Cache::remember('articles.all', 60 * 60, function () {
-        return App\Articles::all();
-    });
+class Articles
+{
+    public function all()
+    {
+        return Cache::remember('articles.all', 60 * 60, function () {
+            return App\Articles::all();
+        });
+    }
+}
+Route::get('/', function (Articles $articles) {
+    return $articles->all();
 });
