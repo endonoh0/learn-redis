9dd7bc9e0a1ff07eebd727c0cba5af31674611c4
Eric Ho <endonoho@gmail.com>
2020-09-29 13:36:56 -0700

    Implement trending article with sorted sets

--- redis-app/routes/web.php
+++ redis-app/routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,17 +11,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('videos/{id}', function ($id) {
-    $downloads = Redis::get("videos.{$id}.downloads");
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Redis;
 
-    return view('welcome')->withDownloads($downloads);
+Route::get('articles/trending', function () {
+    // Get the top 3 trending articles
+    $trending = Redis::zrevrange('trending_articles', 0, 2);
+
+    // Fetch all articles by id
+    App\Article::whereIn('id', $trending);
+
+    // Decode json and rehyrate
+    // hyrdate accepts arr of records, filters each one, creates new instance while passing through the attributes
+    App\Article::hydrate(
+        array_map('json_decode', $trending)
+    );
+
+    // You may also create a global query scope
+    App\Article::hydrateFromJsonString($trending);
+
+    return $trending;
 });
 
-Route::get('videos/{id}/downloads', function ($id) {
-    // Prepare the download.
+Route::get('articles/{article}', function (App\Article $article) {
+    // increment trending_articles score by 1, set member to the article id
+    Redis::zincrby('trending_articles', 1, $article->id);
 
-    // Create namespace seperated by period or semi-colon
-    Redis::incr("videos.{$id}.downloads");
+    // If you don't want to store the id, you can store the article which will be cast to json in redis-cli
+    Redis::zincrby('trending_articles', 1, $article);
 
-    return back();
+    // Remove items from trending_articles sorted set and retain the top 3 items
+    // You can go to kernel.php to set up a schedule redis command that runs once a week/month
+    Redis::zremrangebyrank('trending_articles', 0, -4);
+
+    return $article;
 });
