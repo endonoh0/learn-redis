c775571d11091ab3ff8662c7859d46f7fade61bb
Eric Ho <endonoho@gmail.com>
2020-09-29 15:43:16 -0700

    Wrapped database query in a call to Cache::remember()

--- redis-app/routes/web.php
+++ redis-app/routes/web.php
@@ -3,15 +3,36 @@
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
+// Class decorator that turns caching on and off
+class CacheableArticles
+{
+    protected $articles;
+
+    // constructor gives instance of Articles class
+    public function __construct($articles)
+    {
+        $this->articles = $articles;
+    }
+
+    public function all()
+    {
+        // Decorating logic with caching layer
+        return Cache::remember('articles.all', 60 * 60, function () {
+            return $this->articles->all();
+        });
+    }
+}
+
 class Articles
 {
     public function all()
     {
-        return Cache::remember('articles.all', 60 * 60, function () {
-            return App\Articles::all();
-        });
+        return App\Articles::all();
     }
 }
-Route::get('/', function (Articles $articles) {
+
+Route::get('/', function () {
+    $articles = new CacheableArticles(new Articles);
+
     return $articles->all();
 });
