d215a69320b5b62c6c132c3e69e446168d15622d
Eric Ho <endonoho@gmail.com>
2020-09-29 12:17:11 -0700

    Install redis, Implement visitor counter

---
+++ redis-app/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // increment on visits (key) which defaults to one and then two on each page reload
+    $visits = Redis::incr('visits');
+
+    return view('welcome')->withVisits($visits);
+});
